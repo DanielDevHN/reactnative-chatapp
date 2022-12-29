37af9225c26e7f89ad7fc1beee36fbe3df0fa643
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2022-12-29 09:50:39 -0600

    Uploading project files

---
+++ app.config.js
@@ -0,0 +1,57 @@
+import "dotenv/config";
+
+export default {
+  expo: {
+    name: "ChatApp_ReactNative",
+    slug: "ChatApp_ReactNative",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash3.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF",
+      },
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    extra: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+      eas: {
+        projectId: "f632eeb6-078d-4784-96ef-5a34a5e2be47",
+      },
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.chat.chatapp",
+    },
+    android: {
+      package: "com.chat.chatapp",
+      config: {},
+    },
+    description: "Aplicación de Chat",
+  },
+};
